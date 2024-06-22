0d7ad5dee150e29bfa5b2f8ad6a081443b8f8d6d
ENU <enurinm@gmail.com>
2024-06-22 14:56:38 +0900

    skill 발동조건 테이블 분리

---
+++ src/main/java/com/ras/dao/SkillDao.java
@@ -0,0 +1,12 @@
+package com.ras.dao;
+
+import com.ras.data.Skill;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SkillDao extends Skill {
+}
--- src/main/java/com/ras/data/Skill.java
+++ src/main/java/com/ras/data/Skill.java
@@ -1,27 +1,13 @@
 package com.ras.data;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Skill {
     private Integer id;
     private String name;
     private String img;
     private String txt;
-
-    /**
-     * 발동 조건 종류
-     */
-    private Integer triggerType;
-    private Integer triggerStatId;
-
-    /**
-     * TRIGGER_TYPE이
-     * 1(쿨타임): 기간 (라운드)
-     * 2, 3, 4: 스탯 값 조건
-     */
-    private Integer triggerNum;
-    private boolean triggerPersentYn; // 퍼센트/정수
-    private boolean triggerBeAbYn; // 이상/이하
-    private boolean triggerFirstDoYn; // 최초 발동 시 쿨타임 고려 여부
 }
