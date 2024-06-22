e4ca9f5227d58aa2294e310b7edee6b15a23dbb7
ENU <enurinm@gmail.com>
2024-06-22 15:21:00 +0900

    skill 발동조건 테이블 분리

--- src/main/java/com/ras/dao/SkillDao.java
+++ src/main/java/com/ras/dao/SkillDao.java
@@ -1,12 +1,22 @@
 package com.ras.dao;
 
 import com.ras.data.Skill;
+import com.ras.data.SkillEffect;
+import com.ras.data.SkillTrigger;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class SkillDao extends Skill {
+    // 스킬 발동조건 리스트
+    List<SkillTrigger> skillTriggerList;
+
+    // 스킬 효과 리스트
+    List<SkillEffect> skillEffectList;
+
 }
